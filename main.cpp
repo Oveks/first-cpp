36d668599770be871277ae556e0b3fa722094710
Oveks <140532129+Oveks@users.noreply.github.com>
2023-10-03 21:14:31 -0500

    Test and comment on variables

--- main.cpp
+++ main.cpp
@@ -4,6 +4,25 @@ using namespace std;
 
 int main()
 {
-  cout <<"Hello World!" <<endl;
+  // Single character variable
+  char adj = 'a';
+  // Multi-character variable
+  string name = " Bob";
+  //Integer value (+ or -, no decimal)
+  int age;
+  age = 21; //Another way to define variables (2 lines instead of one)
+  //float and double do the same thing (integers with decimals)
+  float hour = 4.7; //more niche use cases
+  double time = 3.2; //stick with this for now
+
+  //print text mad lib
+  cout <<"Hello" << name << ", said joe mama" << hour << time << endl;
+  cout <<"I am " << adj << " " << age << " years old!" << endl;
+
+  name = " Jim"; //redefine variables
+  age = 69; //redefine variables
+  cout <<"Hello" << name << ", it's joever" << endl;
+  cout <<"I am " << age << "!" << endl;
+
   return 0;
 }
